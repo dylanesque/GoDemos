9e18a06916d29c0b7caff167f87a4b373a10707d
Michael Caveney <dylanesque27@gmail.com>
2021-05-22 15:25:10 -0400

    Began channels & routines

--- http/main.go
+++ http/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://google.com")
@@ -13,5 +16,15 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(resp)
+	// make is a construct in Go that makes an empty data type with x amount of room inside
+	// bs := make([]byte, 9999)
+	lw := logWriter{}
+
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+
+	return len(bs), nil
 }
