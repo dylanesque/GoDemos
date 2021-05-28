0194b706d07d6bcce03b07154e81bdd904047d6d
Michael Caveney <dylanesque27@gmail.com>
2021-05-28 17:32:52 -0400

    Added string reversal solution

--- recipes/main.go
+++ recipes/main.go
@@ -1,13 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
-	fmt.Println(celsToFaren(30))
+	fmt.Println(temperatureConverter(30))
 }
 
-func celsToFaren(i float64) float64 {
-	return i * 9 / 5 + 32;
+func temperatureConverter(i float64) float64 {
+	return i*9/5 + 32
 }
 
-
+// As noted in this SO thread(https://stackoverflow.com/questions/1752414/how-to-reverse-a-string-in-go), reversing a string
+// in Go is far from straightforward.
+func reverseString(s string) string {
+	size := len(s)
+	buf := make([]byte, size)
+	for start := 0; start < size; {
+		r, n := utf8.DecodeRuneInString(s[start:])
+		start += n
+		utf8.EncodeRune(buf[size-start:], r)
+	}
+	return string(buf)
+}
